665ae19b2d12a4b7b4209046fdc198bb439d6ae2
Tony <anthonyshikanga@gmail.com>
2017-07-19 07:51:53 +0300

    *modified circle.js model file with respective attributes

--- app/models/circle.js
+++ app/models/circle.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  name: DS.attr('string'),
+  createdAt: DS.attr('date', {
+    defaultValue() {return new Date();}
+  }),
+  users: DS.hasMany('user'),
+  child: DS.belongsTo('child')
 
 });
